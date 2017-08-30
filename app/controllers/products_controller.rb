fb846db57617f1a58123adf3152c8a7792e9d0ba
DonutJr <cyrus.younessi@gmail.com>
2017-08-29 20:56:28 -0500

    third commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+  def one_product_action
+    @product = Product.last
+    render 'one_product_page.html.erb'
+  end
+
+  def all_products_action
+    @products = Product.all
+    render 'all_products_page.html.erb'
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(
+                      name: "HanSolo",
+                      price: 10,
+                      image: "https://vignette4.wikia.nocookie.net/lego/images/7/78/29307519626_5efb560901_b.jpg/revision/latest?cb=20160901113934",
+                      description: "coolest lego ever")
+
+product.save
+
+product = Product.new(
+                      name: "DaftPunk",
+                      price: 100,
+                      image: "http://cdn.hiconsumption.com/wp-content/uploads/2013/12/LEGO-Daft-Punk-Minifigures.jpg",
+                      description: "funkiest lego ever")
+
+product.save
+
+product = Product.new(
+                      name: "KnightRider",
+                      price: 33,
+                      image: "http://cdn.hiconsumption.com/wp-content/uploads/2013/12/LEGO-Knight-Rider-Set.jpg",
+                      description: "Don't Hassle the Hoff")
+
+product.save
+
+product = Product.new(
+                      name: "BreakingBad",
+                      price: 42,
+                      image: "http://cdn.hiconsumption.com/wp-content/uploads/2013/12/Breaking-Bad-LEGO-Superlab-Set.jpg",
+                      description: "I am the one who knocks.")
+
+product.save
+
+product = Product.new(
+                      name: "Goonies",
+                      price: 300,
+                      image: "http://cdn.hiconsumption.com/wp-content/uploads/2013/12/The-Goonies-30th-Anniversary-LEGO-Set.jpg",
+                      description: "Sloth love Chunk")
+
+product.save
+
+product = Product.new(
+                      name: "RonBurgundy",
+                      price: 400,
+                      image: "http://cdn.hiconsumption.com/wp-content/uploads/2013/12/Ron-Burgundy-LEGO-Minifgure.jpg",
+                      description: "I'm Ron Burgundy?")
+
+product.save
