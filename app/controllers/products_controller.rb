2ad62b6cde8fee50e6586b0c7f5a030fa3ceb944
DonutJr <cyrus.younessi@gmail.com>
2017-09-05 21:17:39 -0500

    capstone 9-5

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,22 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+  end
 
+  def new
+    # @product = Product.new
+  end
+
+  def create
+    product = Product.new(
+                          name: params[:name],
+                          price: params[:price],
+                          image: params[:image],
+                          description: params[:description]
+                          )
+      product.save
+      flash[:success] = "Lego Successfully Created"
+      redirect_to "/products/#{product.id}"
   end
 
   def show
@@ -19,5 +34,35 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+
+    product.assign_attributes(
+                              name: params[:name],
+                              price: params[:price],
+                              image: params[:image],
+                              description: params[:description]
+                              )
+
+    product.name = params[:name]
+    product.price = params[:price]
+    product.image = params[:image]
+    product.description = params[:description]
+
+    product.save
+    flash[:success] = "Lego Successfully Updated"
+    redirect_to "/products/#{product.id}"
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    flash[:danger] = "Lego Successfully Destroyed"
+    redirect_to "/products/"
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,18 @@ Rails.application.routes.draw do
   # get '/all_products_url' => 'products#all_products_action'
   get '/' => 'products#index'
   get '/products' => 'products#index'
+
+  get '/products/new' => 'products#new'
+  post '/products' => 'products#create'
+
+
   get '/products/:id' => 'products#show'
+
+
+
+  get '/products/:id/edit' => 'products#edit'
+  patch 'products/:id' => 'products#update'
+
+  delete '/products/:id' => 'products#destroy'
 
 end
