540b50e73ed72f27b52b55dbc11cd73fad6bf367
DonutJr <cyrus.younessi@gmail.com>
2017-09-06 21:23:03 -0500

    new layout plus tax and total

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  def sale_message
+    "Discount item!" if price < 2
+    "Everyday value!" if price > 2
+  end
+
+  def tax
+    0.09 * price
+  end
+
+  def total
+    price + tax
+  end
+
 end
